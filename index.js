b346430818a1efcdb00b651cc2cf68c94e9647c2
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2023-06-01 17:38:43 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const compression = require('compression');
 const app = express();
 app.use(compression());
 
-const CHUNK_SIZE = 10 * 1024 * 1024; // 10 MB
-
 app.get('/stream/:infoHash/:fileIndex', async (req, res) => {
   const { infoHash, fileIndex } = req.params;
   // Create a torrent stream instance
@@ -31,32 +29,14 @@ app.get('/stream/:infoHash/:fileIndex', async (req, res) => {
       }
 
       const { start, end } = ranges[0];
-      const totalSize = end - start + 1;
 
       res.statusCode = 206;
       res.setHeader('Content-Range', `bytes ${start}-${end}/${file.length}`);
-      res.setHeader('Content-Length', totalSize);
+      res.setHeader('Content-Length', end - start + 1);
 
-      // Stream each chunk separately
-      let chunkStart = start;
-      let chunkEnd = Math.min(chunkStart + CHUNK_SIZE - 1, end);
-
-      while (chunkStart <= end) {
-        res.writeHeader(200, {
-          'Content-Range': `bytes ${chunkStart}-${chunkEnd}/${file.length}`,
-          'Content-Length': chunkEnd - chunkStart + 1,
-          'Content-Type': 'video/mp4',
-        });
-
-        const chunkStream = file.createReadStream({ start: chunkStart, end: chunkEnd });
-        chunkStream.pipe(res);
-
-        // Update chunkStart and chunkEnd for the next chunk
-        chunkStart = chunkEnd + 1;
-        chunkEnd = Math.min(chunkStart + CHUNK_SIZE - 1, end);
-      }
-
-      res.end();
+      // Stream the file directly to the response
+      const fileStream = file.createReadStream({ start, end });
+      fileStream.pipe(res);
     } else {
       res.setHeader('Content-Length', file.length);
       file.createReadStream().pipe(res);
