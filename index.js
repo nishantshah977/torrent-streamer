4751aa56e43af9d8a5918dbd871253666d29269b
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2023-06-07 17:16:24 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -80,6 +80,107 @@ app.get('/stream', (req, res) => {
 
   });
 
+app.get('/download', (req, res) => {
+
+  const magnetURI = req.query.magnet;
+
+  if (!magnetURI) {
+
+    return res.status(400).json({ error: 'Magnet URI is required' });
+
+  }
+
+  // Create a torrent engine
+
+  const engine = torrentStream(magnetURI);
+
+  // Array to store file streams
+
+  const videoStreams = [];
+
+  // When the engine is ready
+
+  engine.on('ready', () => {
+
+    console.log('Engine ready');
+
+    // Filter video files based on file extension
+
+    const videoFiles = engine.files.filter((file) => {
+
+      const ext = file.name.split('.').pop().toLowerCase();
+
+      return ext === 'mp4' || ext === 'mkv' || ext === 'avi'; // Add more extensions if needed
+
+    });
+
+    if (videoFiles.length === 0) {
+
+      return res.status(404).json({ error: 'No video files found in the torrent' });
+
+    }
+
+    // Create a zip archive
+
+    const archive = archiver('zip');
+
+    // Set the appropriate headers for the zip archive
+
+    res.attachment('videos.zip');
+
+    res.setHeader('Content-Type', 'application/octet-stream');
+
+    // Pipe the zip archive to the client's response
+
+    archive.pipe(res);
+
+    // Iterate over video files
+
+    videoFiles.forEach((file) => {
+
+      // Create a read stream for the video file
+
+      const readStream = file.createReadStream();
+
+      // Add the video stream to the array
+
+      videoStreams.push(readStream);
+
+      // Add the video file to the zip archive
+
+      archive.append(readStream, { name: file.name });
+
+    });
+
+    // Finalize the zip archive
+
+    archive.finalize();
+
+  });
+
+  // When the download is complete
+
+  engine.on('download', () => {
+
+    if (engine.swarm.downloaded === engine.swarm.numPeers * engine.torrent.length) {
+
+      console.log('Download complete');
+
+      // Cleanup: Close all video streams
+
+      videoStreams.forEach((stream) => {
+
+        stream.destroy();
+
+      });
+
+    }
+
+  });
+
+});
+
+
   engine.on('error', (err) => {
 
     console.error('Error initializing torrent engine:', err);
